53761cde681dd48b8a129aa9397c821f704e7c52
sagar.maharjan <sagar.maharjan@f1soft.com>
2021-08-14 10:15:17 +0545

    Changed: Project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { SidebarComponent } from './components/main-container/sidebar/sidebar.component';
+import { SidebarContentComponent } from './components/main-container/sidebar/sidebar-content/sidebar-content.component';
+import { ChatAreaComponent } from './components/main-container/chat-area/chat-area.component';
+import { ChatDefaultPageComponent } from './components/main-container/chat-area/chat-default-page/chat-default-page.component';
+import { ChatRoomComponent } from './components/main-container/chat-area/chat-room/chat-room.component';
+import { MaterialModule } from './shared/material.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    SidebarComponent,
+    SidebarContentComponent,
+    ChatAreaComponent,
+    ChatDefaultPageComponent,
+    ChatRoomComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
